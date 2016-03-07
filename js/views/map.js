90c0aebb4f60318623334549bea429be9416fa53
wiigolas <acawiig@gmail.com>
2016-03-07 16:13:39 +0100

    map add

--- js/views/map.js
+++ js/views/map.js
@@ -1,16 +1,9 @@
 var map = L.map('map').locate({setView: true, maxZoom: 13});
-var greenIcon = L.icon({
-    iconUrl: 'c.png',
-
-    iconSize:     [38, 38], // size of the icon
-    iconAnchor:   [19, 38], // point of the icon which will correspond to marker's location
-    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
-});
 
 function onLocationFound(e) {
     var radius = e.accuracy;
 
-    L.marker(e.latlng, {icon: greenIcon}).addTo(map);
+    L.marker(e.latlng).addTo(map);
 
     }   
 
@@ -24,13 +17,13 @@ map.on('locationerror', onLocationError);
 
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    attribution: '',
     maxZoom: 18,
     id: 'wiigolas.p7idlkkp',
     accessToken: 'pk.eyJ1Ijoid2lpZ29sYXMiLCJhIjoiY2lreHYxejNvMDA0NndsbTRmejl4NndqMSJ9.5hfLbJnXbAsfsPRT3V4W4Q'
 }).addTo(map);
 
-
+/*
 $.getJSON('js/restaurants.json', function (json) {
     var restaurang = [];
     for (var key in json) {
@@ -44,3 +37,4 @@ $.getJSON('js/restaurants.json', function (json) {
     }  
 });
 
+*/
