508ea80eb0e36f8e0d549f9df2964f0eba999f44
wiigolas <acawiig@gmail.com>
2016-03-14 13:53:36 +0100

    add leaflet and restaurant get

--- GETPOST/js/getRestaurants.js
+++ GETPOST/js/getRestaurants.js
@@ -1,13 +1,40 @@
- var app = angular.module('app', []);
+var app = angular.module('app', []);
     app.controller('getRestaurants', function($scope, $http) {
-    var voteMap = {};
-        voteMap.user = [];
-        voteMap.restaurant = [];
-        voteMap.vote = [];
-        voteMap.group = [];
-    
-    $http.get("js/json/restaurants.json").success(function(response) {
-        $scope.item = response;
-        var items = response.included;
+    var start = new Date().getTime();
+    var restaurantResult = {};    
+    $http({
+      method: 'GET',
+      url: 'js/json/restaurants.json'
+    }).then(function successCallback(response) {
+        result(response.data);
+    }, function errorCallback(response) {
+       console.log("error");
     });
+        
+    function result(resultData){
+        var items = resultData.data;
+        for(var k in items){
+            var restaurant = items[k];
+            var restaurantData = {
+                "name": restaurant.attributes.name,
+                "lat": restaurant.attributes.latitude,
+                "long":  restaurant.attributes.longitude,
+                "photo": restaurant.attributes.photo,
+                "rating":  restaurant.attributes.rating,
+                "extra": {
+                    "info":  restaurant.attributes.info,
+                    "pricerate":  restaurant.attributes.pricerate,
+                    "number_votes":  restaurant.attributes.number_votes,
+                    "number_votes-won":  restaurant.attributes.number_won_votes,
+                    "categories":  restaurant.relationships.data
+                }
+            }
+            var id = restaurant.id;
+            restaurantResult[id] = restaurantData;
+        }
+         placeMarker(restaurantResult);
+        var end = new Date().getTime();
+        var time = end - start;
+        console.log("Exec time = " + time);
+    }
 });
--- js/views/map.js
+++ js/views/map.js
@@ -12,22 +12,6 @@ map.on('locationfound', onLocationFound);
 function onLocationError(e) {
     alert(e.message);
 }
-//test marker and popup
-map.on('locationerror', onLocationError);
-var marker = L.marker([55.7597, 13.0074]).addTo(map);
-marker.bindPopup("<b>LÖDDEKÖPINGE!</b>").openPopup();
-var marker = L.marker([57.916, 13.877]).addTo(map);
-
-//costum marker with location
-var greenIcon = L.icon({
-    iconUrl: 'images/marker-icon-coffee-red.png',
-
-    iconSize:     [25, 41], // size of the icon
-    iconAnchor:   [12, 41], // point of the icon which will correspond to marker's location
-});
-
-L.marker([51.5, -0.09], {icon: greenIcon}).addTo(map);
-
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     maxZoom: 18,
@@ -35,18 +19,12 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
     accessToken: 'pk.eyJ1Ijoid2lpZ29sYXMiLCJhIjoiY2lreHYxejNvMDA0NndsbTRmejl4NndqMSJ9.5hfLbJnXbAsfsPRT3V4W4Q'
 }).addTo(map);
 
-/*
-$.getJSON('js/restaurants.json', function (json) {
-    var restaurang = [];
-    for (var key in json) {
-        if (json.hasOwnProperty(key)) {   
-                var item = json[key];
-            var info = "<p>Namn: " + item.name + "</p><p>Betyg: " + item.rating + "/5</p><button ng-click='count = count + 1' ng-init='count=0'>Rösta</button>";
-                marker = new L.marker([item.lat,item.long])
-                    .bindPopup(info)
-                    .addTo(map);
-        } 
-    }  
-});
 
-*/
+function placeMarker(json) {
+    console.log(json);
+    for (var key in json) {   
+        var item = json[key];
+        var info = item.name;
+        marker = new L.marker([item.lat,item.long]).bindPopup(info).addTo(map);
+    }  
+};
