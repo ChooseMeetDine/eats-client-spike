d0879142124e20c38b638380be08695e6b2ff748
wiigolas <acawiig@gmail.com>
2016-03-21 13:11:31 +0100

    menyerna

--- js/controllers/slider.js
+++ js/controllers/slider.js
@@ -1,64 +1,17 @@
 app.controller('slideMenu', function ($scope, $timeout, $mdSidenav, $log) {
-    $scope.toggleLeft = buildToggler('left');
-    $scope.toggleRight = buildToggler('right');
-    $scope.isOpenRight = function(){
-      return $mdSidenav('right').isOpen();
-    };
-    $scope.isOpenLeft = function(){
-      return $mdSidenav('left').isOpen();
-    };
-    /**
-     * Supplies a function that will continue to operate until the
-     * time is up.
-     */
-    function debounce(func, wait, context) {
-      var timer;
-      return function debounced() {
-        var context = $scope,
-            args = Array.prototype.slice.call(arguments);
-        $timeout.cancel(timer);
-        timer = $timeout(function() {
-          timer = undefined;
-          func.apply(context, args);
-        }, wait || 10);
-      };
-    }
-    /**
-     * Build handler to open/close a SideNav; when animation finishes
-     * report completion in console
-     */
-    function buildDelayedToggler(navID) {
-      return debounce(function() {
-        $mdSidenav(navID)
-          .toggle()
-          .then(function () {
-            $log.debug("toggle " + navID + " is done");
-          });
-      }, 200);
-    }
+    $scope.toggleMainMenu = buildToggler('main');
+    $scope.toggleFilterMenu = buildToggler('filter');
+    
     function buildToggler(navID) {
       return function() {
         $mdSidenav(navID)
           .toggle()
-          .then(function () {
-            $log.debug("toggle " + navID + " is done");
-          });
+          if(navID == 'main'){
+            $mdSidenav('filter').close();
+          }else{
+            $mdSidenav('main').close();  
+          }
       }
     }
   })
-  .controller('LeftCtrl', function ($scope, $timeout, $mdSidenav, $log) {
-    $scope.close = function () {
-      $mdSidenav('left').close()
-        .then(function () {
-          $log.debug("close LEFT is done");
-        });
-    };
-  })
-  .controller('RightCtrl', function ($scope, $timeout, $mdSidenav, $log) {
-    $scope.close = function () {
-      $mdSidenav('right').close()
-        .then(function () {
-          $log.debug("close RIGHT is done");
-        });
-    };
-  });
+  
--- js/views/map.js
+++ js/views/map.js
@@ -1,13 +1,13 @@
 var map = L.map('map', { zoomControl: false }).locate({setView: true, maxZoom: 13});
 
-new L.Control.Zoom({ position: 'bottomleft' }).addTo(map);
+new L.Control.Zoom({ position: 'topright' }).addTo(map);
 
-new L.control.locate({position: 'bottomleft'}).addTo(map);
+new L.control.locate({position: 'topright'}).addTo(map);
 
 function onLocationFound(e) {
     var radius = e.accuracy;
 
-    L.marker(e.latlng).addTo(map);
+    L.marker(e.latlng, {icon:redIcon}).addTo(map);
 
     }   
 
@@ -18,10 +18,20 @@ function onLocationError(e) {
 }
 //test marker and popup
 map.on('locationerror', onLocationError);
+var marker = L.marker([55.7597, 13.0074]).addTo(map);
+marker.bindPopup("<b>LÖDDEKÖPINGE!</b>").openPopup();
+var marker = L.marker([57.916, 13.877]).addTo(map);
 
 //costum marker with location
 var greenIcon = L.icon({
     iconUrl: 'images/marker-icon-coffee-red.png',
+
+    iconSize:     [25, 41], // size of the icon
+    iconAnchor:   [12, 41], // point of the icon which will correspond to marker's location
+});
+
+var redIcon = L.icon({
+    iconUrl: 'images/marker-icon-red.png',
 
     iconSize:     [25, 41], // size of the icon
     iconAnchor:   [12, 41], // point of the icon which will correspond to marker's location
@@ -36,8 +46,9 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
     accessToken: 'pk.eyJ1Ijoid2lpZ29sYXMiLCJhIjoiY2lreHYxejNvMDA0NndsbTRmejl4NndqMSJ9.5hfLbJnXbAsfsPRT3V4W4Q'
 }).addTo(map);
 
+//Function for creating map markers and marker popup from json data
 function placeMarker(json) {
-    console.log(json);
+    //console.log(json);
     for (var key in json) { 
         var item = json[key];
         var info = '<p>' + item.name + '</p><p>' + item.rating + '</p><img src="' + item.photo + '">';
