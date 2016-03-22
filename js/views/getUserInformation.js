93f80120c2dacb66c13c08357100f627ac47ce2c
wiigolas <acawiig@gmail.com>
2016-03-22 14:18:19 +0100

    user info

--- js/views/getUserInformation.js
+++ js/views/getUserInformation.js
@@ -1,10 +1,31 @@
 app.controller('getUserInformation', function($scope, $http) {
+    
+    var userResult = {}; 
+    
     $http({
       method: 'GET',
-      url: 'js/json/userinformation.json'
+      url: 'js/json/users.json'
     }).then(function successCallback(response) {
-        result(response.data);
+        resultUser(response.data);
     }, function errorCallback(response) {
        console.log("error");
     });
+    
+    function resultUser(resultData){
+        var items = resultData.data;
+        for(var k in items){
+            var user = items[k];
+            
+            var userData = {
+                "id" : user.id,
+                "name" : user.attributes.name,
+                "photo" : user.attributes.photo    
+            }
+            
+            var id = user.id;
+            userResult[id] = userData;
+        }
+    console.log(userResult);
+    }
+    
 });
